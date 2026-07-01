8bfb531aad219d82bdc5d4eed24579bbb13aa9b1
dinabagherzadeh264-db <dinabagherzadeh264@gmail.com>
2026-07-01 14:21:06 +0330

    DexterityEffect & VulnerableEffect & modifyBlock and onTurnEnd functions added

--- StatusEffect.cpp
+++ StatusEffect.cpp
@@ -9,8 +9,28 @@ void StatusEffect::setAmount(int value) { amount = value; }
 
 int StatusEffect::modifyOutgoingDamage(int baseDamage) { return baseDamage; }
 int StatusEffect::modifyIncomingDamage(int baseDamage) { return baseDamage; }
+int StatusEffect::modifyBlock(int baseBlock) { return baseBlock; }
 
 //______________________________________StrengthEffect______________________________________
 StrengthEffect::StrengthEffect(int increaseDamageBy) : StatusEffect("Strength", increaseDamageBy) {}
 
 int StrengthEffect::modifyOutgoingDamage(int baseDamage) { return baseDamage + amount; }
+
+//______________________________________DexterityEffect_____________________________________
+DexterityEffect::DexterityEffect(int increaseBlockBy) : StatusEffect("Dexterity", increaseBlockBy) {}
+
+int DexterityEffect::modifyBlock(int baseBlock) { return baseBlock + amount; }
+
+//_____________________________________VulnerableEffect_____________________________________
+VulnerableEffect::VulnerableEffect(int turns) : StatusEffect("Vulnerable", turns) {}
+
+int VulnerableEffect::modifyIncomingDamage(int baseDamage) {
+    if (amount > 0) 
+        return baseDamage * 1.5; 
+    return baseDamage;
+}
+
+void VulnerableEffect::onTurnEnd() {
+    if (amount > 0) 
+        amount--; 
+}
--- StatusEffect.h
+++ StatusEffect.h
@@ -18,12 +18,27 @@ public:
 
     virtual int modifyOutgoingDamage(int baseDamage);
     virtual int modifyIncomingDamage(int baseDamage);
+    virtual int modifyBlock(int baseBlock) { return baseBlock; }
+    virtual void onTurnEnd() {}
 };
 
 class StrengthEffect : public StatusEffect {
 public:
     StrengthEffect(int increaseDamageBy);
     int modifyOutgoingDamage(int baseDamage) override;
+};
+
+class DexterityEffect : public StatusEffect {
+public:
+    DexterityEffect(int increaseBlockBy);
+    int modifyBlock(int baseBlock) override;
+};
+
+class VulnerableEffect : public StatusEffect {
+public:
+    VulnerableEffect(int turns);
+    int modifyIncomingDamage(int baseDamage) override;
+    void onTurnEnd() override; 
 };
 
 #endif
